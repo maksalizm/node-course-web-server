80aee0bf536e9655b6de5547b3ba82fce35ff52d
maksalizm <maksalizm@gmail.com>
2017-05-28 18:13:48 +0900

    add project page

--- server.js
+++ server.js
@@ -5,35 +5,31 @@ const fs = require('fs');
 const port = process.env.PORT || 3000;
 var app = express();
 
-
-hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
-hbs.registerHelper('getCurrentYear', ()=> {
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
 
 hbs.registerHelper('screamIt', (text) => {
-  return text.toUpperCase()
+  return text.toUpperCase();
 });
-
-app.use((req, res, next) => {
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} : ${req.url}`;
-  console.log(log);
-  fs.appendFile('server.log', log + '\n', (err) => {
-      if (err) {
-          console.log('Unable to append to server.log');
-      }
-  });
-  next();
-});
-
-// app.use('/', (req,res ,next) => {
-//     res.render('maintenance.hbs');
-// });
-
-app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
@@ -45,6 +41,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
